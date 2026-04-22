4d427ef59fd1a557a4c17468c5da7a13a2cdc70b
r muthu varuna <muthuvaruna4@gmail.com>
2026-04-22 11:18:55 +0530

    Add to Cart added

---
+++ Add to Cart application/script.js
@@ -0,0 +1,29 @@
+let inputEle = document.getElementById("cartItemTextInput");
+let btn = document.getElementById("addBtn");
+let delbtn = document.getElementById("Delbtn");
+let ulEl = document.createElement('ul');
+let container = document.getElementById("div");
+container.appendChild(ulEl);
+
+let value = function() {
+    let list = document.createElement('li');
+    if (inputEle.value=== "") {
+        return;
+    }
+    list.textContent = inputEle.value;
+    ulEl.appendChild(list);
+    inputEle.value = "";
+};
+/*let delValue = function() {
+    //if (ulEl.lastChild) {
+        ulEl.removeChild(ulEl.lastChild);
+    }
+};*/
+let delValue = function() {
+    if (ulEl.lastElementChild) {
+        ulEl.removeChild(ulEl.lastElementChild);
+    }
+};
+
+btn.addEventListener("click", value);
+delbtn.addEventListener("click", delValue);
